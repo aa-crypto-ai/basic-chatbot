10caac5eb134eaac6716d042e8208e18fa8e38e3
aa.crypto.ai <39453357+aa-crypto-ai@users.noreply.github.com>
2025-02-27 19:03:06 +0800

    Add more models for ChatGPT, Gemini, Llama, Mistral

--- app/model_list.py
+++ app/model_list.py
@@ -3,9 +3,25 @@ models = {
         'display_name': "OpenAI - ChatGPT 4o mini",
         'cost': (0.15, 0.6, 7.225),
     },
+    "openai/gpt-4o-2024-11-20": {
+        'display_name': "OpenAI - ChatGPT 4o",
+        'cost': (2.5, 10, 3.613),
+    },
+    "openai/o3-mini": {
+        'display_name': "OpenAI - ChatGPT o3 mini",
+        'cost': (1.1, 4.4, None),
+    },
+    "openai/o3-mini-high": {
+        'display_name': "OpenAI - ChatGPT o3 mini high",
+        'cost': (1.1, 4.4, None),
+    },
     "google/gemini-2.0-flash-lite-001": {
         'display_name': "Google Gemini 2.0 Flash Lite",
         'cost': (0.075, 0.3, None),
+    },
+    "google/gemini-2.0-flash-001": {
+        'display_name': "Google Gemini 2.0 Flash",
+        'cost': (0.1, 0.4, 0.0258),
     },
     "anthropic/claude-3.7-sonnet": {
         'display_name': "Claude Sonnet 3.7",
@@ -15,8 +31,12 @@ models = {
         'display_name': "Claude Sonnet 3.7 (thinking)",
         'cost': (3, 15, 4.8),
     },
-    "meta-llama/llama-3.1-70b-instruct": {
-        'display_name': "Llama 3.1 70B",
+    "meta-llama/llama-3.3-70b-instruct": {
+        'display_name': "Llama 3.3 70B",
         'cost': (0.12, 0.3, None),
+    },
+    "mistralai/mistral-small-24b-instruct-2501": {
+        'display_name': 'Mistral: Mistral Small 3 24B',
+        'cost': (0.07, 0.14, None),
     },
 }
