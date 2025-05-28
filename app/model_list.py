4f216edcda51fa69770617aab82939fcc2ae32fc
aa.crypto.ai <39453357+aa-crypto-ai@users.noreply.github.com>
2025-05-28 22:07:42 +0800

    Model list - add sonnet 4, opus 4, gemini 2.5, and chatgpt models 4.1 and o4

--- app/model_list.py
+++ app/model_list.py
@@ -3,6 +3,22 @@ models = {
         'display_name': "OpenAI - ChatGPT 4o mini",
         'cost': (0.15, 0.6, 7.225),
     },
+    "openai/gpt-4.1": {
+        'display_name': "OpenAI - ChatGPT 4.1",
+        'cost': (2, 8, None),
+    },
+    # "openai/o3": {
+    #     'display_name': "OpenAI - ChatGPT o3",
+    #     'cost': (10, 40, 7.65),
+    # },
+    "openai/o4-mini-high": {
+        'display_name': "OpenAI - ChatGPT o4 mini high",
+        'cost': (1.10, 4.40, 0.842),
+    },
+    "openai/o4-mini": {
+        'display_name': "OpenAI - ChatGPT o4 mini",
+        'cost': (1.10, 4.40, 0.842),
+    },
     "openai/gpt-4o-2024-11-20": {
         'display_name': "OpenAI - ChatGPT 4o",
         'cost': (2.5, 10, 3.613),
@@ -15,6 +31,10 @@ models = {
         'display_name': "OpenAI - ChatGPT o3 mini high",
         'cost': (1.1, 4.4, None),
     },
+    "google/gemini-2.5-pro-preview": {
+        'display_name': "Google Gemini 2.5 Pro Preview",
+        'cost': (1.25, 10, 5.16),
+    },
     "google/gemini-2.0-flash-lite-001": {
         'display_name': "Google Gemini 2.0 Flash Lite",
         'cost': (0.075, 0.3, None),
@@ -22,6 +42,14 @@ models = {
     "google/gemini-2.0-flash-001": {
         'display_name': "Google Gemini 2.0 Flash",
         'cost': (0.1, 0.4, 0.0258),
+    },
+    "anthropic/claude-opus-4": {
+        'display_name': "Claude Opus 4",
+        'cost': (15, 75, 24),
+    },
+    "anthropic/claude-sonnet-4": {
+        'display_name': "Claude Sonnet 4",
+        'cost': (3, 15, 4.8),
     },
     "anthropic/claude-3.7-sonnet": {
         'display_name': "Claude Sonnet 3.7",
